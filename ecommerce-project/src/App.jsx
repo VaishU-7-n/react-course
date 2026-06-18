c644d92af485a6cc5a8cbf6333984b887a79d74e
vaishnavi <nelantivaishnavi@gmail.com>
2026-06-18 13:28:47 +0530

    generate the html for home page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,6 +4,7 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Routes, Route } from 'react-router';
 import { TrackPackage } from './pages/TrackPackage'
 import { OrdersPage } from './pages/OrdersPage';
+import { NotFound } from './pages/NotFound'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path = "checkout" element={<CheckoutPage />}></Route>
       <Route path = "orders" element = {<OrdersPage/>}></Route>
       <Route path = "tracking" element = {<TrackPackage/>}></Route>
+      <Route path = "*" element = {<NotFound/>}></Route>
     </Routes>
   
   )
---
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../components/header'
+
+export function NotFound()
+{
+    return(
+        <>
+        <Header></Header>
+        <h1>Page Not Found</h1>
+        </>
+    );
+}
