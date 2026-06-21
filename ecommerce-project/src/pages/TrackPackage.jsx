fdd492189a35e25e9284db79b2427f5b56e26b67
vaishnavi <nelantivaishnavi@gmail.com>
2026-06-21 23:09:33 +0530

    convert home and checkout to async await

--- ecommerce-project/src/pages/TrackPackage.jsx
+++ ecommerce-project/src/pages/TrackPackage.jsx
@@ -1,41 +1,32 @@
-import { Header } from '../components/Header'
-import './TrackPackage.css'
-import { Link } from 'react-router';
-export function TrackPackage()
-{
-    return(
-   
-        <div class="tracking-page">
-        <Header></Header>
+import { Header } from "../components/Header";
+import "./TrackPackage.css";
+import { Link } from "react-router";
+export function TrackPackage() {
+  return (
+    <div class="tracking-page">
+      <Header></Header>
       <div class="order-tracking">
-        <Link class ="back-to-orders-link link-primary" to="/orders">
+        <Link class="back-to-orders-link link-primary" to="/orders">
           View all orders
         </Link>
 
-        <div class="delivery-date">
-          Arriving on Monday, June 13
-        </div>
+        <div class="delivery-date">Arriving on Monday, June 13</div>
 
         <div class="product-info">
           Black and Gray Athletic Cotton Socks - 6 Pairs
         </div>
 
-        <div class="product-info">
-          Quantity: 1
-        </div>
+        <div class="product-info">Quantity: 1</div>
 
-        <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+        <img
+          class="product-image"
+          src="images/products/athletic-cotton-socks-6-pairs.jpg"
+        />
 
         <div class="progress-labels-container">
-          <div class="progress-label">
-            Preparing
-          </div>
-          <div class="progress-label current-status">
-            Shipped
-          </div>
-          <div class="progress-label">
-            Delivered
-          </div>
+          <div class="progress-label">Preparing</div>
+          <div class="progress-label current-status">Shipped</div>
+          <div class="progress-label">Delivered</div>
         </div>
 
         <div class="progress-bar-container">
@@ -43,6 +34,5 @@ export function TrackPackage()
         </div>
       </div>
     </div>
-    );
-
+  );
 }
